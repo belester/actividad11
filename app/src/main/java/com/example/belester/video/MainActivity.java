34c4b2bcc2856e9c0707f26e73f183f5c1c906b9
Unknown <franciscobelester@gmail.com>
2018-10-19 17:51:16 -0500

    video

---
+++ app/src/main/java/com/example/belester/video/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.belester.video;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Binding
+        mVideoView = findViewById(R.id.videoview);
+
+        //Set Video View to Media Controller
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(mVideoView);
+
+        //Set Media Controller to Video View
+        mVideoView.setMediaController(controller);
+    }
+
+    private void initializePlayer(){
+        mVideoView.setVideoPath("https://www.ebookfrenzy.com/android_book/movie.mp4");
+        mVideoView.start();
+    }
+
+    private void releasePlayer() {mVideoView.stopPlayback();}
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        releasePlayer();
+    }
+}
